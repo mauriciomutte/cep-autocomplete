68365215b90834575971420fca06111646b428f9
Mauricio Mutte <litiobr@gmail.com>
2017-08-08 16:38:12 -0300

    Map error alert

--- js/javascript.js
+++ js/javascript.js
@@ -49,24 +49,28 @@ function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 17,
     center: {lat: -33.724, lng: -16.031}
-  });
-  var geocoder = new google.maps.Geocoder();
+  })
+  var geocoder = new google.maps.Geocoder()
   document.getElementById('submit').addEventListener('click', function() {
-    geocodeAddress(geocoder, map);
-  });
+    geocodeAddress(geocoder, map)
+  })
 }
 function geocodeAddress(geocoder, resultsMap) {
-  var address = document.getElementById('rua').value + ', ' + document.getElementById('cidade').value 
-  geocoder.geocode({'address': address}, function(results, status) {
-    if (status === google.maps.GeocoderStatus.OK) {
-      resultsMap.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-        map: resultsMap,
-        position: results[0].geometry.location
-      });
-    } else {
-    	document.getElementById('address').style.borderColor = "#e00012";
-      alert('Desculpe, não encontramos seu CEP');
-    }
-  });
+  var address = document.getElementById('rua').value + ' ' + document.getElementById('cidade').value
+  if (address != ' ') {
+	  geocoder.geocode({'address': address}, function(results, status) {
+	    if (status === google.maps.GeocoderStatus.OK) {
+	      resultsMap.setCenter(results[0].geometry.location)
+	      var marker = new google.maps.Marker({
+	        map: resultsMap,
+	        position: results[0].geometry.location
+	      })
+	    } else {
+	    	document.getElementById('address').style.borderColor = "#e00012"
+	      alert('Desculpe, não encontramos seu CEP')
+	    }
+	  })
+  } else {
+  	alert('Endereço não encontrado!')
+  }
 }
