2fc1aa1697fb44720e615a76a2b544f2d77166ee
Maurício Mutte <litiobr@gmail.com>
2019-02-11 21:47:22 -0200

    Add gulp

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    server: {
+      baseDir: './',
+    }
+  });
+});
