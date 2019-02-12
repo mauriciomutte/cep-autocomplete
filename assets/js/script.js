fe97506af640f8725e2b692f3c2c22cc703d4013
Maurício Mutte <litiobr@gmail.com>
2019-02-11 22:00:07 -0200

    Remove CEP mask

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,3 @@
-function formatar(mascara, documento){
-	var i = documento.value.length
-	var saida = mascara.substring(0,1)
-	var texto = mascara.substring(i)
-	if (texto.substring(0,1) != saida){
-		documento.value += texto.substring(0,1)
-	}
-}
-
 function errorCep() {
 	let rua = document.getElementById('rua').value = ('')
 	let bairro = document.getElementById('bairro').value = ('')
